894dadc51b1c0f09b633a92f1863a3945f411549
Leilani Allen <leilani.allen90@gmail.com>
2021-09-18 15:54:32 -0700

    Passing 9/14 tests

--- binary_search_tree/tree.py
+++ binary_search_tree/tree.py
@@ -14,40 +14,128 @@ class Tree:
     def __init__(self):
         self.root = None
 
-    # Time Complexity: 
-    # Space Complexity: 
-    def add(self, key, value = None):
-        pass
+    def add_helper(self, current_node, key, value):
+        if current_node == None:
+            return TreeNode(key, value)
 
-    # Time Complexity: 
-    # Space Complexity: 
+        if key <= current_node.key:
+            current_node.left = self.add_helper(current_node.left, key, value)
+        else:
+            current_node.right = self.add_helper(current_node.right, key, value)
+        return current_node
+        
+
+
+    # Time Complexity: O(log n)
+    # Space Complexity: O(1)
+    def add(self, key, value=None):
+        if self.root == None:
+            self.root = TreeNode(key, value)
+        else:
+            self.add_helper(self.root, key, value)
+
+    # Time Complexity: O(log n)
+    # Space Complexity: O(1)
     def find(self, key):
-        pass
+        if self.root == None:
+            return None
 
-    # Time Complexity: 
-    # Space Complexity: 
+        current = self.root
+        while current != None:
+            if current.key == key:
+                return current.value
+            elif current.key < key:
+                current = current.right
+            else:
+                current = current.left
+        return None
+
+# IN ORDER LIST*************************************************************
+    # Time Complexity: O(log n)
+    # Space Complexity: O(1)
+    def inorder_helper(self, current_node, inorder_list):
+        if current_node == None:
+            return inorder_list
+
+        # ADD LEFT NODE FIRST
+        self.inorder_helper(current_node.left, inorder_list)
+
+        # ADD CURRENT NODE
+        inorder_list.append({ 
+            "key": current_node.key,
+            "value": current_node.value
+        })
+        # ADD RIGHT NODE LAST
+        self.inorder_helper(current_node.right, inorder_list)
+
+        return inorder_list
+
     def inorder(self):
-        pass
+        inorder_list = []
+        return self.inorder_helper(self.root, inorder_list)
 
-    # Time Complexity: 
-    # Space Complexity:     
+
+# PRE-ORDER LIST*************************************************************
+
+    # Time Complexity: O(log n)
+    # Space Complexity: O(1)    
+    def preorder_helper(self, current_node, preorder_list):
+        if current_node == None:
+            return preorder_list
+
+        # ADD CURRENT NODE FIRST 
+        preorder_list.append({ 
+            "key": current_node.key,
+            "value": current_node.value
+        })
+        # ADD LEFT NODE, THEN RIGHT NODE
+        self.preorder_helper(current_node.left, preorder_list)
+        self.preorder_helper(current_node.right, preorder_list)
+
+        return preorder_list
+
     def preorder(self):
-        pass
+        preorder_list = []
+        return self.preorder_helper(self.root, preorder_list)
 
-    # Time Complexity: 
-    # Space Complexity:     
+
+
+# POST-ORDER LIST*************************************************************
+
+    # Time Complexity: O(log n)
+    # Space Complexity:  O(1)   
+    def postorder_helper(self, current_node, postorder_list):
+        if current_node == None:
+            return postorder_list
+
+        # ADD LEFT NODE, THEN RIGHT NODE 
+        self.postorder_helper(current_node.left, postorder_list)
+        self.postorder_helper(current_node.right, postorder_list)
+
+        # ADD CURRENT NODE LAST 
+        postorder_list.append({ 
+            "key": current_node.key,
+            "value": current_node.value
+        })
+        return postorder_list
+
     def postorder(self):
-        pass
+        postorder_list = []
+        return self.postorder_helper(self.root, postorder_list)
 
-    # Time Complexity: 
-    # Space Complexity:     
+
+
+
+
+    # Time Complexity: O(log n)
+    # Space Complexity:  O(1)   
     def height(self):
         pass
 
 
 #   # Optional Method
-#   # Time Complexity: 
-#   # Space Complexity: 
+#   # Time Complexity: O(log n)
+#   # Space Complexity: O(1)
     def bfs(self):
         pass
 
