c0b1e756fcd94b5ff010253e703ba7092d091e21
mairandomness <mairakodama@gmail.com>
2021-09-19 15:03:04 -0700

    Add solutions

--- binary_search_tree/tree.py
+++ binary_search_tree/tree.py
@@ -9,50 +9,124 @@ class TreeNode:
         self.right = None
         
 
-
 class Tree:
     def __init__(self):
         self.root = None
 
-    # Time Complexity: 
-    # Space Complexity: 
+    # Time Complexity: O(logn)
+    # Space Complexity: O(1)
     def add(self, key, value = None):
-        pass
+        new_node = TreeNode(key, value)
+        if not self.root:
+            self.root = new_node
+            return None
 
-    # Time Complexity: 
-    # Space Complexity: 
+        curr = self.root
+
+        while True:
+            if curr.key > key and curr.left is None:
+                curr.left = new_node
+                return None
+            elif curr.key <= key and curr.right is None:
+                curr.right = new_node
+                return None
+            elif curr.key > key:
+                curr = curr.left
+            
+            else:
+                curr = curr.right
+
+    # Time Complexity: O(logn)
+    # Space Complexity: O(1)
     def find(self, key):
-        pass
+        curr = self.root
 
-    # Time Complexity: 
-    # Space Complexity: 
+        while curr:
+            if curr.key == key:
+                return curr.value
+            elif curr.key > key:
+                curr = curr.left
+            else:
+                curr = curr.right
+        return None 
+        
+
+    # Time Complexity: O(n) with n being the number of nodes
+    # Space Complexity: O(n)
     def inorder(self):
-        pass
+        if not self.root:
+            return []
 
-    # Time Complexity: 
-    # Space Complexity:     
+        left_tree = Tree()
+        left_tree.root = self.root.left
+        right_tree = Tree()
+        right_tree.root = self.root.right
+
+        return left_tree.inorder() + [{"key": self.root.key, "value": self.root.value}] + right_tree.inorder()
+        
+
+    # Time Complexity: O(n) with n being the number of nodes
+    # Space Complexity: O(n)   
     def preorder(self):
-        pass
+        if not self.root:
+            return []
 
-    # Time Complexity: 
-    # Space Complexity:     
+        left_tree = Tree()
+        left_tree.root = self.root.left
+        right_tree = Tree()
+        right_tree.root = self.root.right
+
+        return [{"key": self.root.key, "value": self.root.value}] + left_tree.preorder() + right_tree.preorder()
+
+    # Time Complexity: O(n) with n being the number of nodes
+    # Space Complexity: O(n)    
     def postorder(self):
-        pass
+        if not self.root:
+            return []
 
-    # Time Complexity: 
-    # Space Complexity:     
+        left_tree = Tree()
+        left_tree.root = self.root.left
+        right_tree = Tree()
+        right_tree.root = self.root.right
+
+        return left_tree.postorder() + right_tree.postorder() + [{"key": self.root.key, "value": self.root.value}]
+
+    # Time Complexity: O(n) with n being the number of nodes
+    # Space Complexity: O(n)      
     def height(self):
-        pass
+        if not self.root:
+            return 0
+
+        left_tree = Tree()
+        left_tree.root = self.root.left
+        right_tree = Tree()
+        right_tree.root = self.root.right
+
+        return max(left_tree.height() , right_tree.height()) + 1
 
 
 #   # Optional Method
-#   # Time Complexity: 
-#   # Space Complexity: 
+    # Time Complexity: O(n) with n being the number of nodes
+    # Space Complexity: O(n) 
     def bfs(self):
-        pass
+        bfs_array = []
+        if not self.root:
+            return []
+        curr_level = [self.root]
+        while curr_level:
+            bfs_array += [{"key": node.key, "value": node.value} for node in curr_level]
+            curr_level = self.get_next_level(curr_level)
+        return bfs_array
 
-        
+    def get_next_level(self, curr_level):
+        next_level = []
+        for node in curr_level:
+            if node.left:
+                next_level.append(node.left)
+            if node.right:
+                next_level.append(node.right)
 
+        return next_level
 
 #   # Useful for printing
     def to_s(self):
