6f08ecaf972fa4bb8dccc587c918544e1fc63ab6
Ruthie Newman <ruthienewman@protonmail.com>
2021-09-23 12:07:32 -0700

    Added traversal methods

--- binary_search_tree/tree.py
+++ binary_search_tree/tree.py
@@ -70,16 +70,23 @@ class Tree:
         return None
 
 
+    def inorder_helper(self):
+        pass
+
     # Time Complexity: 
     # Space Complexity: 
     def inorder(self):
     #left, root, right
         nodes_array = []
+        current =self.root
 
-        if self.root != None:
-            nodes_array = self.inorder(self.root.left)
-            nodes_array.append(self.root.value)
-            nodes_array = nodes_array + self.inorder(self.root.right) 
+        if self.root == None:
+            return []
+
+        while current != None:
+            nodes_array = self.inorder(current.left)
+            nodes_array.append(current.value)
+            nodes_array = nodes_array + self.inorder(current.right) 
         return nodes_array 
 
     # Time Complexity: 
@@ -89,11 +96,11 @@ class Tree:
 
         #array to record traversed nodes
         nodes_array = []
-        
-        if self.root != None: 
-            nodes_array.append(self.root.value)
-            nodes_array = self.preorder(self.root.left)
-            nodes_array = self.preorder(self.root.right)
+        root = self.root
+        while root != None: 
+            nodes_array.append(root.value)
+            nodes_array.append(self.preorder(root.left.value))
+            nodes_array.append(self.preorder(root.right.value))
         return nodes_array
 
 
@@ -107,25 +114,33 @@ class Tree:
             nodes_array.append(self.postorder(self.root.right))
         return nodes_array
 
+    def height_helper(self):
+        pass
 
     # Time Complexity: 
     # Space Complexity:     
-    def height(self, root):
-        if self.root == None:
-            return None
+    def height(self):
+
+        root = self.root
+        if root == None:
+            return 0
         
-        left = self.height(self.root.left)
-        right = self.height(self.root.right)
+        return 1 + max(self.height(root.left), self.height(root.right))
+        
+        #other possibilites...
+        # left = self.height(root.left)
+        # right = self.height(root.right)
 
-        if left > right:
-            return left + 1
-        else:
-            return right + 1
+        # if left > right:
+        #     return left + 1
+        # else:
+        #     return right + 1
 
-    #if the current root is nil/none 
-    #return None 
-    #else 
-    #return 1 + the maximum of the heoght of the left and right subtrees
+        #concept:
+        #if the current root is nil/none 
+        #return None 
+        #else 
+        #return 1 + the maximum of the heoght of the left and right subtrees
 
 
 #   # Optional Method
@@ -140,22 +155,23 @@ class Tree:
         return f"{self.inorder()}"
 
 tree = Tree()
-print(tree.root == None)
+# print(tree.root == None)
 tree.add(5, 'Ruthie')
 tree.add(7, 'Ada')
 tree.add(10)
 tree.add(3)
 tree.add(4)
 
-print(tree.root.key == 5)
-print(tree.root.right.key == 7)
-print(tree.root.right.right.key == 10)
-# print(tree.root.left.key == 3)
-print(tree.root.left.right.key == 4)
+# print(tree.root.key == 5)
+# print(tree.root.right.key == 7)
+# print(tree.root.right.right.key == 10)
+# # print(tree.root.left.key == 3)
+# print(tree.root.left.right.key == 4)
 
-print(tree.find(5) == "Ruthie")
-print(tree.find(7) == 'Ada')
-print(tree.find(4) == 4)
+# print(tree.find(5) == "Ruthie")
+# print(tree.find(7) == 'Ada')
+# print(tree.find(4) == 4)
+print(tree.height_helper())
 
 
 # def delete_node(self, key):
