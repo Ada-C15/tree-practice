be48fbcdd899152d6c725fff00b8819b6e1409b0
brew doctor <brew upgrade python@3.9>
2021-09-26 21:07:08 -0700

    NguyenMichelle Binary Search Trees - Tree Exercise

--- binary_search_tree/tree.py
+++ binary_search_tree/tree.py
@@ -6,45 +6,124 @@ class TreeNode:
         self.key = key
         self.value = val
         self.left = None
-        self.right = None
-        
+        self.right = None  
 
 
 class Tree:
     def __init__(self):
         self.root = None
 
-    # Time Complexity: 
-    # Space Complexity: 
+    def add_helper(self, current_node, key, value):
+        if current_node == None:
+            return TreeNode(key, value)
+
+        if key <= current_node.key:
+            current_node.left = self.add_helper(current_node.left, key, value)
+        else:
+            current_node.right = self.add_helper(current_node.right, key, value)
+
+        return current_node
+
+    # Time Complexity: O(log n) - if balanced
+    # Space Complexity: O(n) eventually O(log n) - recursive solution, "addresses are removed from stack when returning"
     def add(self, key, value = None):
-        pass
+        if self.root == None:
+            self.root = TreeNode(key, value)
+        else:
+            self.add_helper(self.root, key, value)
 
-    # Time Complexity: 
-    # Space Complexity: 
+    # Time Complexity: O(log n)
+    # Space Complexity: O(n) eventually O(log n) - recursive solution, "addresses are removed from stack when returning"
     def find(self, key):
-        pass
+        if self.root == None:
+            return None
+        
+        current = self.root
+        while current != None:
+            if current.key == key:
+                return current.value
+            elif current.key < key:
+                current = current.right
+            else:
+                current = current.left
+        
+        return None
 
-    # Time Complexity: 
-    # Space Complexity: 
+    # Inorder helper function  
+    def inorder_helper(self, current, traversal_list):
+        if current != None:
+            self.inorder_helper(current.left, traversal_list)
+            traversal_list.append({'key': current.key, 'value': current.value})
+            self.inorder_helper(current.right, traversal_list)
+
+    # NOTE I've been reading different arguments for recursive tree traversals online
+    # and am now unsure of the most "correct" answer
+
+    # Time Complexity: O(n) - recursive tree traversals go through each node?
+    # Space Complexity: O(h) for height of tree, O(n) in worst case of skewed tree, O(log n) for nice balanced tree
     def inorder(self):
-        pass
+        traversal_list = []
+        self.inorder_helper(self.root, traversal_list)
 
-    # Time Complexity: 
-    # Space Complexity:     
+        return traversal_list
+
+    # Preorder helper function  
+    def preorder_helper(self, current, traversal_list):
+        if current != None:
+            traversal_list.append({'key': current.key, 'value': current.value})
+            self.preorder_helper(current.left, traversal_list)
+            self.preorder_helper(current.right, traversal_list)
+
+    # Time Complexity: O(n)
+    # Space Complexity: O(h) for height of tree, O(n) in worst case of skewed tree, O(log n) for nice balanced tree  
     def preorder(self):
-        pass
+        traversal_list = []
+        self.preorder_helper(self.root, traversal_list)
 
-    # Time Complexity: 
-    # Space Complexity:     
+        return traversal_list
+
+    # Postorder helper function  
+    def postorder_helper(self, current, traversal_list):
+        if current != None:
+            self.postorder_helper(current.left, traversal_list)
+            self.postorder_helper(current.right, traversal_list)
+            traversal_list.append({'key': current.key, 'value': current.value})
+                
+
+    # Time Complexity: O(n)
+    # Space Complexity: O(h) for height of tree, O(n) in worst case of skewed tree, O(log n) for nice balanced tree   
     def postorder(self):
-        pass
+        traversal_list = []
+        self.postorder_helper(self.root, traversal_list)
 
-    # Time Complexity: 
-    # Space Complexity:     
+        return traversal_list
+
+    # Referenced GeeksforGeeks and Sunday CS w/Ace for the following:
+
+    # Height helper function   
+    def height_helper(self, current):
+        if current == None:
+            return 0
+
+        left_depth = self.height_helper(current.left)
+        right_depth = self.height_helper(current.right)
+
+        # if left_depth > right_depth:
+        #     return left_depth + 1
+        # else:
+        #     return right_depth + 1
+
+        return max(left_depth, right_depth) + 1
+
+    # Time Complexity: O(n) - still visiting each node?
+    # Space Complexity: O(h) for height of tree, O(n) in worst case of skewed tree, O(log n) for nice balanced tree     
     def height(self):
-        pass
 
+        return self.height_helper(self.root)
 
+        
+
+        
 #   # Optional Method
 #   # Time Complexity: 
 #   # Space Complexity: 
