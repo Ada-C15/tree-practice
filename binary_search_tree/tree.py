731b93cd5839ef5b24bf7cb9a605a3d96e1b686e
mana <manushka@pm.me>
2022-01-25 15:25:15 -0800

    finishes functions

--- binary_search_tree/tree.py
+++ binary_search_tree/tree.py
@@ -9,40 +9,129 @@ class TreeNode:
         self.right = None
         
 
-
 class Tree:
     def __init__(self):
         self.root = None
 
+
+    def add_helper(self, current_node, new_node):
+        
+        if current_node.key > new_node.key:
+            if current_node.left == None:
+                current_node.left = new_node
+            else:
+                self.add_helper(current_node.left, new_node)
+        else:
+            if current_node.right == None:
+                current_node.right = new_node
+            else:
+                self.add_helper(current_node.right, new_node)
+         
+    
     # Time Complexity: 
     # Space Complexity: 
     def add(self, key, value = None):
-        pass
+
+        if self.root == None:
+            self.root = TreeNode(key, value)
+        else:
+            new_node = TreeNode(key, value)
+            self.add_helper(self.root, new_node)    
+
+
+    def find_helper(self, current_node, key):
+        
+        if current_node == None:
+            return None
+
+        if current_node.key == key:
+            return current_node
+
+        if current_node.key > key:
+            return self.find_helper(current_node.left, key)
+        else:
+            return self.find_helper(current_node.right, key)
+
 
     # Time Complexity: 
     # Space Complexity: 
     def find(self, key):
-        pass
+        return self.find_helper(self.root, key).value
 
+
+    def inorder_helper(self, current_node, inorder_return):
+        if current_node == None:
+            return inorder_return
+
+        self.inorder_helper(current_node.left, inorder_return)
+        inorder_return.append({ 
+            "key": current_node.key,
+            "value": current_node.value
+        })
+        self.inorder_helper(current_node.right, inorder_return)
+
+        return inorder_return 
+        
     # Time Complexity: 
     # Space Complexity: 
     def inorder(self):
-        pass
+        inorder_return = []
+        return self.inorder_helper(self.root, inorder_return)
+
+
+    def preorder_helper(self, current_node, preorder_return):
+        if current_node == None:
+            return preorder_return
+
+        preorder_return.append({ 
+            "key": current_node.key,
+            "value": current_node.value
+        })
+        self.preorder_helper(current_node.left, preorder_return)        
+        self.preorder_helper(current_node.right, preorder_return)
+
+        return preorder_return
 
     # Time Complexity: 
     # Space Complexity:     
     def preorder(self):
-        pass
+        preorder_return = []
+        return self.preorder_helper(self.root, preorder_return)
+
+
+    def postorder_helper(self, current_node, postorder_return):
+        if current_node == None:
+            return postorder_return
+
+        self.postorder_helper(current_node.left, postorder_return)        
+        self.postorder_helper(current_node.right, postorder_return)
+        postorder_return.append({ 
+            "key": current_node.key,
+            "value": current_node.value
+        })
+
+        return postorder_return
 
     # Time Complexity: 
     # Space Complexity:     
     def postorder(self):
-        pass
+        postorder_return = []
+        return self.postorder_helper(self.root, postorder_return)
+
+
+    def height_helper(self, current_node):
+        if current_node == None:
+            return 0
+        
+        left_node = self.height_helper(current_node.left)
+        right_node = self.height_helper(current_node.right)
+
+        return max(left_node, right_node) + 1
 
     # Time Complexity: 
     # Space Complexity:     
     def height(self):
-        pass
+        return self.height_helper(self.root)
 
 
 #   # Optional Method
@@ -50,8 +139,6 @@ class Tree:
 #   # Space Complexity: 
     def bfs(self):
         pass
-
-        
 
 
 #   # Useful for printing
